053d155e409164695424cbaca15511a818165895
kaylaZqx <3045817709@qq.com>
2023-08-04 09:59:08 +0800

    commit

--- bite-c/test_8_3/test_8_3/test.c
+++ bite-c/test_8_3/test_8_3/test.c
@@ -2,35 +2,65 @@
 
 #include <stdio.h>
 
+//int main()
+//{
+//	//测试无const
+//	int num = 10;
+//	num = 20;
+//	printf("%d\n", num);
+//
+//	const int num1 = 10;
+//	int* p = &num1;
+//	*p = 20;
+//	printf("%d\n", num1);
+//	
+//	//const 修饰指针变量
+//	//1.const放在*的左边
+//	//意思是：p指向的对象不能通过p来改变，但是p变量本身的值可以改变
+//	//*p = 20;出错
+//	const int num2 = 10;
+//	int n = 100;
+//	//const int* p = &num2;
+//	int const* p = &num2;
+//	*p = 20;//err
+//	p = &n;//ok
+//	printf("%d\n", num2);
+//	//2.const放在*的右边
+//	//意思是：p指向的对象是可以通过p来改变的，但是不能修改p变量本身的值
+//	const int num3 = 10;
+//	int *const p = &num3;
+//	*p = 0;//ok
+//	p = &num3;//err
+//	printf("%d\n", num3);
+//	return 0;
+//}
+
 int main()
 {
-	//测试无const
-	int num = 10;
-	num = 20;
-	printf("%d\n", num);
+	int arr1[] = { 1,2,3,4,5 };
+	int arr2[] = { 2,3,4,5,6 };
+	int arr3[] = { 3,4,5,6,7 };
+	int* parr[3] = { arr1,arr2,arr3 };
 
-	const int num1 = 10;
-	int* p = &num1;
-	*p = 20;
-	printf("%d\n", num1);
-	
-	//const 修饰指针变量
-	//1.const放在*的左边
-	//意思是：p指向的对象不能通过p来改变，但是p变量本身的值可以改变
-	//*p = 20;出错
-	const int num2 = 10;
-	int n = 100;
-	//const int* p = &num2;
-	int const* p = &num2;
-	*p = 20;//err
-	p = &n;//ok
-	printf("%d\n", num2);
-	//2.const放在*的右边
-	//意思是：p指向的对象是可以通过p来改变的，但是不能修改p变量本身的值
-	const int num3 = 10;
-	int *const p = &num3;
-	*p = 0;//ok
-	p = &num3;//err
-	printf("%d\n", num3);
+	int i = 0;
+	for (i = 0; i < 3; i++)
+	{
+		int j = 0;
+		for (j = 0; j < 5; j++)
+		{
+			printf("%d ", *(parr[i] + j));//*(parr[i]+j)==parr[i][j]
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+int main()
+{
+	int arr[10] = { 0 };
+	int* p = arr;
+	int(*p2)[10] = &arr;
+
+
 	return 0;
 }
