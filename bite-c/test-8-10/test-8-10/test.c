c5bd305f1ff2fd4c89a46c9fb7d7e10bc5b51546
kaylaZqx <3045817709@qq.com>
2023-08-11 15:47:17 +0800

    commit

--- bite-c/test-8-10/test-8-10/test.c
+++ bite-c/test-8-10/test-8-10/test.c
@@ -1,42 +1,225 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 #include <assert.h>
 
+////方法1：
+//char* my_strcpy(char* dest,const char* sour)
+//{
+//	assert(dest != NULL);
+//	assert(sour != NULL);
+//	char* ret = dest;
+//	while (*sour != '\0')
+//	{
+//		*dest = *sour;
+//		sour++;
+//		dest++;
+//	}
+//	*dest = *sour;
+//	return ret;
+//
+//}
+//
+////方法1：简化
+//char* my_strcpy(char* dest, const char* sour)
+//{
+//	assert(dest && sour);
+//	char* ret = dest;
+//	while (*dest++ == *sour++)
+//	{
+//		;
+//	}
+//	return ret;
+//
+//}
+//
+//int main()
+//{
+//	char arr1[] = "abcdef";
+//	char arr2[20] = { 0 };
+//	my_strcpy(arr2, arr1);
+//	printf("%s\n", arr2);
+//	return 0;
+//}
+
+
 //方法1：
-char* my_strcpy(char* dest,const char* sour)
+//int my_strcmp(const char* str1, const char* str2)
+//{
+//	assert(str1 && str2);
+//	while (*str1 == *str2)
+//	{
+//		if (*str1 == '\0')
+//		{
+//			return 0;
+//		}
+//		str1++;
+//		str2++;
+//	}
+//	if (*str1 > *str2)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return -1;
+//	}
+//}
+
+
+////方法1：简化
+//int my_strcmp(const char* str1, const char* str2)
+//{
+//	assert(str1 && str2);
+//	while (*str1 == *str2)
+//	{
+//		if (*str1 == '\0')
+//		{
+//			return 0;
+//		}
+//		str1++;
+//		str2++;
+//	}
+//	return (*str1 - *str2);
+//}
+//
+//int main()
+//{
+//	char arr1[] = "abcdef";
+//	char arr2[] = "abc";
+//	int ret = my_strcmp(arr1, arr2);
+//	if (ret > 0)
+//	{
+//		printf(">\n");
+//	}
+//	else if (ret == 0)
+//	{
+//		printf("=\n");
+//	}
+//	else
+//	{
+//		printf(">\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr1[20] = "Hello\0xxxxx";
+//	char arr2[] = "bit";
+//	strncat(arr1, arr2, 6);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
+
+//char* my_strstr(const char* str1, const char* str2)
+//{
+//	assert(str1 && str2);
+//	const char* s1 = str1;
+//	const char* s2 = str2;
+//	const char* p = str1;
+//
+//	while (*p)
+//	{
+//		s1 = p;
+//		s2 = str2;
+//		while ((*s1 != '\0') && (*s2 != '\0') && (*s1 == *s2))
+//		{
+//			s1++;
+//			s2++;
+//		}
+//		if (*s2 == '\0')
+//		{
+//			return (char*)p;
+//		}
+//		p++;
+//	}
+//	return NULL;
+//}
+//
+//int main()
+//{
+//	char arr1[] = "abbbcef";
+//	char arr2[] = "bbc";
+//	char* ret = my_strstr(arr1, arr2);
+//	if (ret == NULL)
+//	{
+//		printf("子串不存在\n");
+//	}
+//	else
+//	{
+//		printf("%s\n", ret);
+//	}
+//	return 0;
+//}
+
+
+
+
+//void* my_memcpy(void* dest, const void* src, size_t num)
+//{
+//	assert(dest && src);
+//	void* ret = dest;
+//
+//	while (num--)
+//	{
+//		*(char*)dest = *(char*)src;
+//		dest = (char*)dest + 1;
+//		src = (char*)src + 1;
+//	}
+//
+//	return ret;
+//}
+//
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5,6,7 };
+//	int arr2[10] = { 0 };
+//	my_memcpy(arr2, arr1, 28);
+//	
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr2[i]);
+//	}
+//	return 0;
+//}
+
+
+void* my_memmove(void* dest, const void* src, size_t num)
 {
-	assert(dest != NULL);
-	assert(sour != NULL);
+	assert(dest && src);
 	char* ret = dest;
-	while (*sour != '\0')
+	if (dest < src)
 	{
-		*dest = *sour;
-		sour++;
-		dest++;
+		//前----》后
+		while (num--)
+		{
+			*(char*)dest = *(char*)src;
+			dest = (char*)dest + 1;
+			src = (char*)src + 1;
+		}
 	}
-	*dest = *sour;
-	return ret;
-
-}
-
-//方法1：简化
-char* my_strcpy(char* dest, const char* sour)
-{
-	assert(dest && sour);
-	char* ret = dest;
-	while (*dest++ == *sour++)
+	else
 	{
-		;
+		//后------》前
+		while (num--)
+		{
+			*((char*)dest + num) = *((char*)src + num);
+		}
 	}
 	return ret;
-
 }
 
 int main()
 {
-	char arr1[] = "abcdef";
-	char arr2[20] = { 0 };
-	my_strcpy(arr2, arr1);
-	printf("%s\n", arr2);
+	int arr1[] = { 1,2,3,4,5,6,7,8,9,10 };
+	my_memmove(arr1 + 2, arr1, 20);
+
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", arr1[i]);
+	}
 	return 0;
 }
