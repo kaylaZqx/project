42603fa1c8af2001c312badbafc74a0040f775f1
kaylaZqx <3045817709@qq.com>
2023-08-21 16:22:12 +0800

    提交

--- bite-c/homework-8-15/homework-8-15/test.c
+++ bite-c/homework-8-15/homework-8-15/test.c
@@ -108,35 +108,156 @@
 //4.字符串左旋
 //实现一个函数，可以左旋字符串中的k个字符
 //e.g.:   ABCD左旋两个字符CDAB
-#include <string.h>
+//#include <string.h>
 
-void left_rotate(char arr[],int k)
+//方法1：
+//void left_rotate(char arr[],int k)
+//{
+//	int i = 0;
+//	int len = strlen(arr);
+//	k %= len;
+//	for (i = 0; i < k; i++)
+//	{
+//		//旋转1个字符
+//		//1
+//		char tmp = arr[0];
+//		//2
+//		int j = 0;
+//		for (j = 0; j < len - 1; j++)
+//		{
+//			arr[j] = arr[j + 1];
+//		}
+//		//3
+//		arr[len - 1] = tmp;
+//	}
+//}
+
+
+//方法2：
+//#include <assert.h>
+//void reverse(char* left,char* right)
+//{
+//	assert(left && right);
+//	while (left < right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//void left_rotate(char arr[], int k)
+//{
+//	int len = strlen(arr);
+//	k %= len;
+//	reverse(arr, arr + k - 1);//左
+//	reverse(arr + k, arr + len - 1);//右
+//	reverse(arr, arr + len - 1);//整个
+//}
+//
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	int k = 0;
+//	scanf("%d", &k);
+//	left_rotate(arr, k);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+
+
+//4.杨氏矩阵
+//有一个数学矩阵，矩阵的每行从左到右是递增的，矩阵从上到下是递增的，请编写程序在这样的矩阵中查找某个数字是否存在
+//要求：时间复杂度小于O(N)
+
+//方法1：
+struct point
 {
-	int i = 0;
-	int len = strlen(arr);
-	k %= len;
-	for (i = 0; i < k; i++)
+	int x; 
+	int y;
+};
+
+//struct point find_num(int arr[3][3], int r, int c, int k)
+//{
+//	int x = 0;
+//	int y = c - 1;
+//	struct point p = { -1,-1 };
+//	while (x <= r - 1 && y >= 0)
+//	{
+//		if (k < arr[x][y])
+//		{
+//			y--;
+//		}
+//		else if (k > arr[x][y])
+//		{
+//			x++;
+//		}
+//		else
+//		{
+//			p.x = x;
+//			p.y = y;
+//			return p;
+//		}
+//	}
+//	return p;
+//}
+//
+//int main()
+//{
+//	int arr[3][3] = { 1,2,3,4,5,6,7,8,9 };
+//	int k = 0;
+//	scanf("%d", &k);
+//	struct point ret = find_num(arr, 3, 3, k);
+//	printf("%d %d\n", ret.x, ret.y);
+//	return 0;
+//}
+
+//方法2：
+int find_num(int arr[3][3], int *px, int *py, int k)
+{
+	int x = 0;
+	int y = *py - 1;
+	struct point p = { -1,-1 };
+	while (x <= *px - 1 && y >= 0)
 	{
-		//旋转1个字符
-		//1
-		char tmp = arr[0];
-		//2
-		int j = 0;
-		for (j = 0; j < len - 1; j++)
+		if (k < arr[x][y])
 		{
-			arr[j] = arr[j + 1];
+			y--;
 		}
-		//3
-		arr[len - 1] = tmp;
+		else if (k > arr[x][y])
+		{
+			x++;
+		}
+		else
+		{
+			*px = x;
+			*py = y;
+			return 1;
+		}
 	}
+	*px = -1;
+	*py = -1;
+	return 0;
 }
 
 int main()
 {
-	char arr[] = "abcdef";
+	int arr[3][3] = { 1,2,3,4,5,6,7,8,9 };
 	int k = 0;
 	scanf("%d", &k);
-	left_rotate(arr, k);
-	printf("%s\n", arr);
+	int a = 3;//行
+	int b = 3;//列
+	int ret = find_num(arr, &a, &b, k);
+	if (ret == 1)
+	{
+		printf("%d %d\n", a, b);
+	}
+	else
+	{
+		printf("找不到\n");
+	}
 	return 0;
 }
